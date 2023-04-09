a7d25f0ecebe65c4da235a1dd4175f22526a3eab
ValdryanIvandito <valdryan08@gmail.com>
2023-04-09 17:09:36 +0700

    change directory name

--- learn-nodejs/wpu-sandhikagalih/5-thirdparty-module/app.js
+++ learn-nodejs/wpu-sandhikagalih/5-thirdparty-module/app.js
@@ -10,5 +10,7 @@ console.log(validator.isMobilePhone('0226748353', 'id-ID'));
 console.log(validator.isNumeric('0956335453565'));
 console.log(validator.isNumeric('DA3425'));
 
-console.log(chalk.blue('Hello World!'));
-console.log(chalk.red.bgGreenBright(`What is your name?`));
+const message = 'Hello World!';
+console.log(chalk.blue(message));
+console.log(chalk.white.bgBlack(message));
+console.log(chalk.italic.blueBright.bgYellowBright(message));
